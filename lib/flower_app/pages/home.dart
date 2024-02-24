a48f833acce2aa17b641755e54a1e228ab5bb3dc
reda ahmed <reda.ahmed.2032@gmail.com>
2024-02-24 15:41:12 +0200

    Drawer

--- lib/flower_app/pages/home.dart
+++ lib/flower_app/pages/home.dart
@@ -7,46 +7,94 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      actions: [
-        Row(
-          children: [
-            Stack(
-              children: [
-                //  cart count
-                Positioned(
-                  bottom: 24,
-                  child: Container(
-                      padding: const EdgeInsets.all(5),
-                      decoration: const BoxDecoration(
-                          color: Color.fromARGB(211, 164, 255, 193),
-                          shape: BoxShape.circle),
-                      child: const Text(
-                        "8",
+        drawer: Drawer(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  const UserAccountsDrawerHeader(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("assets/img/test.jpg"),
+                          fit: BoxFit.cover),
+                    ),
+                    currentAccountPicture: CircleAvatar(
+                        radius: 55,
+                        backgroundImage: AssetImage("assets/img/ali.jpg")),
+                    accountEmail: Text("ali@yahoo.com"),
+                    accountName: Text("ali Hassan",
                         style: TextStyle(
-                            fontSize: 16, color: Color.fromARGB(255, 0, 0, 0)),
-                      )),
+                          color: Color.fromARGB(255, 255, 255, 255),
+                        )),
+                  ),
+                  ListTile(
+                      title: const Text("Home"),
+                      leading: const Icon(Icons.home),
+                      onTap: () {}),
+                  ListTile(
+                      title: const Text("My products"),
+                      leading: const Icon(Icons.add_shopping_cart),
+                      onTap: () {}),
+                  ListTile(
+                      title: const Text("About"),
+                      leading: const Icon(Icons.help_center),
+                      onTap: () {}),
+                  ListTile(
+                      title: const Text("Logout"),
+                      leading: const Icon(Icons.exit_to_app),
+                      onTap: () {}),
+                ],
+              ),
+              Container(
+                margin: const EdgeInsets.only(bottom: 12),
+                child: const Text("Developed by Ali Hassan © 2022",
+                    style: TextStyle(fontSize: 16)),
+              )
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          actions: [
+            Row(
+              children: [
+                Stack(
+                  children: [
+                    //  cart count
+                    Positioned(
+                      bottom: 24,
+                      child: Container(
+                          padding: const EdgeInsets.all(5),
+                          decoration: const BoxDecoration(
+                              color: Color.fromARGB(211, 164, 255, 193),
+                              shape: BoxShape.circle),
+                          child: const Text(
+                            "8",
+                            style: TextStyle(
+                                fontSize: 16,
+                                color: Color.fromARGB(255, 0, 0, 0)),
+                          )),
+                    ),
+                    // add_shopping_cart
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.add_shopping_cart),
+                    ),
+                  ],
                 ),
-                // add_shopping_cart
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.add_shopping_cart),
+                // value 13 usd
+                const Padding(
+                  padding: EdgeInsets.only(right: 12),
+                  child: Text(
+                    "\$ 13",
+                    style: TextStyle(fontSize: 18),
+                  ),
                 ),
               ],
             ),
-            // value 13 usd
-            const Padding(
-              padding: EdgeInsets.only(right: 12),
-              child: Text(
-                "\$ 13",
-                style: TextStyle(fontSize: 18),
-              ),
-            ),
           ],
-        ),
-      ],
-      backgroundColor: appbarGreen,
-      title: const Text("Home"),
-    ));
+          backgroundColor: appbarGreen,
+          title: const Text("Home"),
+        ));
   }
 }
