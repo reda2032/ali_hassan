59fe999e4cb981f9d952e23fdd6c63c0587ff559
reda ahmed <reda.ahmed.2032@gmail.com>
2024-02-27 21:51:29 +0200

    Firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:ali_hassan/firebase_options.dart';
 import 'package:ali_hassan/flower_app/pages/home.dart';
 import 'package:ali_hassan/flower_app/provider/cart_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
