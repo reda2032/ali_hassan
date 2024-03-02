238d22283364f059f040f7dea2fc37b9c31fbc76
reda ahmed <reda.ahmed.2032@gmail.com>
2024-03-02 18:20:46 +0200

    update StreamBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:ali_hassan/firebase_options.dart';
 import 'package:ali_hassan/flower_app/pages/home.dart';
 import 'package:ali_hassan/flower_app/pages/login.dart';
 import 'package:ali_hassan/flower_app/provider/cart_provider.dart';
+import 'package:ali_hassan/flower_app/shared/snackbar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -32,10 +33,17 @@ class MyApp extends StatelessWidget {
         home: StreamBuilder(
           stream: FirebaseAuth.instance.authStateChanges(),
           builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Home();
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                  child: CircularProgressIndicator(
+                color: Colors.white,
+              ));
+            } else if (snapshot.hasError) {
+              return showSnackBar(context, "Something went wrong");
+            } else if (snapshot.hasData) {
+              return const Home();
             } else {
-              return Login();
+              return const Login();
             }
           },
         ),
